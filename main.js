578dd539ba1be43529875061d8cfd42a9075a42a
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-13 10:26:48 +0100

    creazione di funzioni generiche per muovere lo slider, codice ordinato

--- main.js
+++ main.js
@@ -3,88 +3,95 @@ var post = $(".next");
 var circle = $(".fas.fa-circle")
 console.log(circle);
 
-
-function backword(){
-
-  var imgActive = $(".images img.active");
-
-  if (!imgActive.hasClass("first")) {
-
-    imgActive.removeClass("active");
-    imgActive.prev().addClass("active");
-
-  } else {
-     var lastImg = $("img.last")
-
-     imgActive.removeClass("active");
-     lastImg.addClass("active");
-  }
-
-  var circleActive = $(".fas.fa-circle.active")
-
-  if (!circleActive.hasClass("first")) {
-
-    circleActive.removeClass("active");
-    circleActive.prev().addClass("active");
-
-  } else {
-    var lastCircle = $(".fas.fa-circle.last")
-
-    circleActive.removeClass("active");
-    lastCircle.addClass("active");
-  }
-
-}
-
-function forward (){
-
-  var imgActive = $(".images img.active");
-
-  if (!imgActive.hasClass("last")) {
-
-    imgActive.removeClass("active");
-    imgActive.next().addClass("active");
-
-  } else {
-     var firstImg = $("img.first")
-
-     imgActive.removeClass("active");
-     firstImg.addClass("active");
-  }
-
-  var circleActive = $(".fas.fa-circle.active")
-
-  if (!circleActive.hasClass("last")) {
-
-    circleActive.removeClass("active");
-    circleActive.next().addClass("active");
-
-  } else {
-    var firstCircle = $(".fas.fa-circle.first")
-
-    circleActive.removeClass("active");
-    firstCircle.addClass("active");
-  }
-
-}
-
-prev.click(
-  function (){
-
-    backword();
-
-  }
-);
-
-
-post.click(
-  function (){
-
-    forward();
-
-  }
-);
-
+//4 funzioni diverse per i vari elementi//
+// function backword(){
+//
+//   var imgActive = $(".images img.active");
+//
+//   if (!imgActive.hasClass("first")) {
+//
+//     imgActive.removeClass("active");
+//     imgActive.prev().addClass("active");
+//
+//   } else {
+//      var lastImg = $("img.last");
+//
+//      imgActive.removeClass("active");
+//      lastImg.addClass("active");
+//   }
+//
+// }
+//
+// function bulletBackword (){
+//   var circleActive = $(".fas.fa-circle.active");
+//
+//   if (!circleActive.hasClass("first")) {
+//
+//     circleActive.removeClass("active");
+//     circleActive.prev().addClass("active");
+//
+//   } else {
+//     var lastCircle = $(".fas.fa-circle.last");
+//
+//     circleActive.removeClass("active");
+//     lastCircle.addClass("active");
+//   }
+// }
+//
+// function forward (){
+//
+//   var imgActive = $(".images img.active");
+//
+//   if (!imgActive.hasClass("last")) {
+//
+//     imgActive.removeClass("active");
+//     imgActive.next().addClass("active");
+//
+//   } else {
+//      var firstImg = $("img.first")
+//
+//      imgActive.removeClass("active");
+//      firstImg.addClass("active");
+//   }
+//
+// }
+//
+// function bulletforword (){
+//   var circleActive = $(".fas.fa-circle.active");
+//
+//   if (!circleActive.hasClass("last")) {
+//
+//     circleActive.removeClass("active");
+//     circleActive.next().addClass("active");
+//
+//   } else {
+//     var firstCircle = $(".fas.fa-circle.first")
+//
+//     circleActive.removeClass("active");
+//     firstCircle.addClass("active");
+//   }
+//
+// }
+//
+// prev.click(
+//   function (){
+//
+//     backword();
+//     bulletBackword();
+//
+//   }
+// );
+//
+//
+// post.click(
+//   function (){
+//
+//     forward();
+//     bulletforword();
+//
+//   }
+// );
+//prima funzione di prova//
 // post.click(
 //   function(){
 //     var circleActive = $(".fas.fa-circle.active")
@@ -103,8 +110,84 @@ post.click(
 //   }
 // );
 
+// $(document).keydown(
+//
+//   function(e){
+//
+//     if (e.which == 39){
+//
+//       forward();
+//       bulletforword();
+//
+//     } else if (e.which == 37) {
+//
+//       backword();
+//       bulletBackword();
+//
+//     }
+//   }
+//
+// );
+
+////// SOLO 2 FUNZIONI GENERICHE ////////
+
+function generalForward(activElement, firstElement) {
+
+  if (!activElement.hasClass("last")) {
+
+    activElement.removeClass("active");
+    activElement.next().addClass("active");
+
+  } else {
+
+     activElement.removeClass("active");
+     firstElement.addClass("active");
+  }
+
+}
+
+function generalBackword(activElement, lastElement) {
+
+  if (!activElement.hasClass("first")) {
+
+    activElement.removeClass("active");
+    activElement.prev().addClass("active");
+
+  } else {
+
+     activElement.removeClass("active");
+     lastElement.addClass("active");
+  }
+
+}
 
 
+prev.click(
+  function (){
+    var imgActive = $(".images img.active");
+    var lastImg = $("img.last");
+    generalBackword(imgActive, lastImg);
+
+    var circleActive = $(".fas.fa-circle.active");
+    var lastCircle = $(".fas.fa-circle.last");
+    generalBackword(circleActive, lastCircle);
+  }
+);
+
+
+post.click(
+  function (){
+    var circleActive = $(".images img.active");
+    var firstCircle = $("img.first");
+    generalForward(circleActive, firstCircle);
+
+    var bulletActive = $(".fas.fa-circle.active");
+    var firstBullet = $(".fas.fa-circle.first");
+    generalForward(bulletActive, firstBullet);
+  }
+);
+
+//funzione per click dei bullet//
 circle.click(
   function (){
     var circleActive = $(".fas.fa-circle.active");
